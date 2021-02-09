ab6eab5b6cdc1e153cc7e9e9431196c9c06de97d
Shirasawa <764798966@qq.com>
2021-02-09 16:07:46 +0800

    Update

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # NekoAgent
 
+## Features
+
+- Allow sand duplication.
+- Prevent obsidian spikes reset.
+- Prevent obsidian platform of the_end world generate.
+
 ## Usage
 
 ```bash
 java -javaagent:NekoAgent-1.0-SNAPSHOT.jar -jar paper.jar
 ```
 
-## Disable some functions
+## Disable some features
 
 ```bash
-java -javaagent:NekoAgent-1.0-SNAPSHOT.jar=allowSandDuplication -jar paper.jar
-java -javaagent:NekoAgent-1.0-SNAPSHOT.jar=allowSandDuplication+allowEndPlatform -jar paper.jar
+java -javaagent:NekoAgent-1.0-SNAPSHOT.jar=disallowSandDuplication -jar paper.jar
+java -javaagent:NekoAgent-1.0-SNAPSHOT.jar=allowObsidianSpikesReset -jar paper.jar
+java -javaagent:NekoAgent-1.0-SNAPSHOT.jar=disallowSandDuplication+allowEndPlatform -jar paper.jar
 ```
 
 ## Build
--- src/main/java/cn/apisium/nekoagent/Main.java
+++ src/main/java/cn/apisium/nekoagent/Main.java
@@ -9,20 +9,28 @@ import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 public final class Main {
-    private static boolean allowSandDuplication, allowEndPlatform;
+    private static boolean disallowSandDuplication, allowEndPlatform, allowObsidianSpikesReset;
 
     private final static ClassPool pool = ClassPool.getDefault();
     public static void premain(final String agentArgs, final Instrumentation inst) {
         if (agentArgs != null) {
-            if (agentArgs.contains("allowDuplicateSand")) allowSandDuplication = true;
+            if (agentArgs.contains("disallowSandDuplication")) disallowSandDuplication = true;
             if (agentArgs.contains("allowEndPlatform")) allowEndPlatform = true;
+            if (agentArgs.contains("allowObsidianSpikesReset")) allowObsidianSpikesReset = true;
         }
         inst.addTransformer(new Transformer());
     }
 
     private static String classPrefix;
-    private static String getNMSClassName(final String name) {
-        return classPrefix + name;
+
+    private static String buildDesc(String returnVal, String ...args) {
+        StringBuilder sb = new StringBuilder("(");
+        for (String name : args) {
+            if (name.contains("/")) sb.append('L').append(name);
+            else sb.append('L').append(classPrefix).append(name);
+            sb.append(';');
+        }
+        return sb.append(')').append(returnVal == null ? 'V' : returnVal).toString();
     }
 
     private static final class Transformer implements ClassFileTransformer {
@@ -36,34 +44,48 @@ public final class Main {
                 final CtClass clazz;
                 switch (arr[arr.length - 1]) {
                     case "EntityFallingBlock": {
-                        if (allowSandDuplication) return null;
+                        if (disallowSandDuplication) return null;
                         pool.insertClassPath(new LoaderClassPath(loader));
                         clazz = pool.get(className.replace('/', '.'));
+                        final boolean[] flag = {false};
                         clazz.getMethod("tick", "()V").instrument(new ExprEditor() {
                             public void edit(FieldAccess m) throws CannotCompileException {
                                 if (!m.isReader() || !m.getFieldName().equals("dead")) return;
                                 m.replace("$_ = false;");
+                                flag[0] = true;
                             }
                         });
-                        System.out.println("[NekoAgent] Class EntityFallingBlock modified!");
+                        if (flag[0]) System.out.println("[NekoAgent] Class EntityFallingBlock modified!");
                         break;
                     }
                     case "EntityPlayer":
                         if (allowEndPlatform) return null;
                         pool.insertClassPath(new LoaderClassPath(loader));
                         clazz = pool.get(className.replace('/', '.'));
-                        clazz.getMethod("a", String.format("(L%s;L%s;)V", getNMSClassName("WorldServer"), getNMSClassName("BlockPosition"))).setBody("{}");
+                        clazz.getMethod("a", buildDesc(null, "WorldServer", "BlockPosition")).setBody("{}");
                         System.out.println("[NekoAgent] Class EntityPlayer modified!");
                         break;
                     case "WorldServer":
                         if (allowEndPlatform) return null;
                         pool.insertClassPath(new LoaderClassPath(loader));
                         clazz = pool.get(className.replace('/', '.'));
-                        clazz.getMethod("a", String.format("(L%s;L%s;)V", getNMSClassName("WorldServer"), getNMSClassName("Entity"))).setBody("{}");
+                        clazz.getMethod("a", buildDesc(null, "WorldServer", "Entity")).setBody("{}");
                         System.out.println("[NekoAgent] Class WorldServer modified!");
+                        break;
+                    case "WorldGenEnder":
+                        if (allowObsidianSpikesReset) return null;
+                        pool.insertClassPath(new LoaderClassPath(loader));
+                        clazz = pool.get(className.replace('/', '.'));
+                        clazz.getMethod("a", buildDesc("Z",
+                                "GeneratorAccessSeed", "ChunkGenerator", "java/util/Random", "BlockPosition",
+                                "WorldGenFeatureEndSpikeConfiguration")).setBody("{ return true; }");
+                        clazz.getMethod("a", buildDesc("Ljava/util/List;",
+                                "GeneratorAccessSeed")).setBody("{ return java.util.Collections.emptyList(); }");
+                        System.out.println("[NekoAgent] Class WorldGenEnder modified!");
                         break;
                     default: return null;
                 }
+
                 return clazz.toBytecode();
             } catch (Exception e) {
                 e.printStackTrace();
